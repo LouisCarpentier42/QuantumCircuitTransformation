846b21770965ed936a11f657c11f79592cb47754
Louis Carpentier <louis.carpentier.business@gmail.com>
2019-12-14 17:50:26 +0100

    Added GateIsBlockedException

---
+++ QuantumCircuitTransformation/Exceptions/GateIsBlockedException.cs
@@ -0,0 +1,27 @@
+﻿using QuantumCircuitTransformation.QuantumCircuitComponents.Gates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantumCircuitTransformation.Exceptions
+{
+    /// <summary>
+    ///     GateIsBlockedException:
+    ///         An exception which can be used a try is made to add 
+    ///         an gate to the physical circuit, even though it is 
+    ///         blocked by some other gate. 
+    /// </summary>
+    /// <remarks>
+    ///     @author:   Louis Carpentier
+    ///     @version:  1.0
+    /// </remarks>
+    class GateIsBlockedException : Exception
+    {
+        public GateIsBlockedException() { }
+
+        public GateIsBlockedException(PhysicalGate gate)
+            : base("Gate '" + gate.ToString() + "' is blocked") { }
+
+        public GateIsBlockedException(string message) : base(message) { }
+    }
+}
